5d10510b4d31ab373dbe2ca342afc6d8d9487a11
Ayanabha123456 <ayanabhajana@gmail.com>
2023-02-25 19:55:35 +0000

    Integrated signin and registration functionalities with the html templates

--- ITECH/musicial/models.py
+++ ITECH/musicial/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+
+    age = models.IntegerField(blank=False)
+    gender = models.CharField(max_length=50,blank=False)
+    picture = models.ImageField(upload_to='profile_images',blank=False)
+
+    def __str__(self):
+        return self.user.username
+
+
+
+#any form field validators
--- ITECH/musicial/urls.py
+++ ITECH/musicial/urls.py
@@ -5,6 +5,7 @@ app_name ='musicial'
 
 urlpatterns = [
     path('',views.index,name='index'),
-    path('signin',views.signInPage,name='sigin'),
-    path('register',views.registerPage,name='register')
+    path('signin',views.signInPage,name='signin'),
+    path('register',views.registerPage,name='register'),
+    path('landing',views.userHomepage,name='landing'),
 ]
