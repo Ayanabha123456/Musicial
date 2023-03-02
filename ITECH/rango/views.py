a4201abaf3269b435b1cb14e9029f6a5e266cf16
JunGaoUoG <2768270g@student.gla.ac.uk>
2023-03-02 21:40:21 +0000

    add the views

--- ITECH/rango/views.py
+++ ITECH/rango/views.py
@@ -7,20 +7,19 @@ from rango.forms import LoginForm
 
 def login(request):
 
-    # if request.method == "GET":
-    #     form = LoginForm()
-    #     return render(request, "login.html", {'form': form})
+    if request.method == "GET":
+        form = LoginForm()
+        return render(request, "login.html", {'form': form})
 
-    # form = LoginForm(data=request.POST)
-    # if form.is_valid():
-    #     print(form.cleaned_data)
-    #     admin_object = models.Admin.objects.filter(**form.cleaned_data).first()
-    #     if not admin_object:
-    #         form.add_error("password","用户名或密码错误")
-    #         return render(request, "login.html", {'form': form})
+    form = LoginForm(data=request.POST)
+    if form.is_valid():
+        print(form.cleaned_data)
+        admin_object = models.Admin.objects.filter(**form.cleaned_data).first()
+        if not admin_object:
+            form.add_error("password","用户名或密码错误")
+            return render(request, "login.html", {'form': form})
 
-    #     request.session["info"] = {'id': admin_object.id, 'name': admin_object.username}
-    #     # return redirect("")
-    #     return HttpResponse("校验成功")
-    # return render(request, "login.html", {'form': form})
-    return render(request, "login.html")
+        request.session["info"] = {'id': admin_object.id, 'name': admin_object.username}
+        # return redirect("")
+        return HttpResponse("校验成功")
+    return render(request, "login.html", {'form': form})
